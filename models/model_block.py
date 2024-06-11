08d176ada59f65cd4a82dfb34a2abed18aa9d0be
Your Name <you@example.com>
2024-06-11 20:47:44 +0700

    paper accurate

--- models/model_block.py
+++ models/model_block.py
@@ -165,7 +165,7 @@ class FasterNetBlock(nn.Module):
                  layer_scale_init_value=0,
                  act_layer=nn.GELU,
                  norm_layer=nn.BatchNorm2d,
-                 pconv_fw_type='slicing'
+                 pconv_fw_type='split_cat'
                  ):
 
         super(FasterNetBlock, self).__init__()
